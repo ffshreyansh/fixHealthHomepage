224203dd3c71e57fa50057b89edf056fe8b5fd8f
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2024-01-19 02:23:18 +0530

    form done

--- components/consultation.jsx
+++ components/consultation.jsx
@@ -5,7 +5,6 @@ import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 
-
 const ConsultationForm = () => {
     const [formData, setFormData] = useState({
         name: '',
@@ -17,6 +16,8 @@ const ConsultationForm = () => {
         previousExperience: '',
     });
 
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
     const handleChange = (field, value) => {
         setFormData({
             ...formData,
@@ -25,10 +26,21 @@ const ConsultationForm = () => {
     };
 
     const handleSubmit = () => {
-        // You can perform form submission logic here
-        console.log(formData);
+        if (
+            formData.name &&
+            formData.phoneNumber &&
+            formData.age &&
+            formData.city &&
+            formData.company &&
+            formData.chiefComplaints
+        ) {
+            // You can perform additional validation if needed
+            setFormSubmitted(true);
+            console.log('Form data submitted:', formData);
+        } else {
+            alert('Please fill in all the required fields.');
+        }
     };
-
     const fadeInUpVariants = {
         hidden: { opacity: 0, y: 30 },
         visible: { opacity: 1, y: 0 },
@@ -44,6 +56,7 @@ const ConsultationForm = () => {
 
     return (
         <div className='flex flex-col items-center gap-5 w-full py-20' ref={ref}>
+
             <motion.span className='text-[#00acc1] border border-[#00acc1] text-xs rounded-full px-2 py-1 '
                 variants={fadeInUpVariants}
                 initial="hidden"
@@ -64,10 +77,20 @@ const ConsultationForm = () => {
             >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id ratione nihil eos voluptatem adipisci fugit modi impedit perferendis accusamus culpa?</motion.p>
 
             <div className='flex flex-col lg:flex-row items-center justify-between w-full'>
-                <div className='flex flex-col items-center gap-3 text-white w-full p-3 lg:p-10'>
+                <div className='flex flex-col  items-center gap-3 text-white w-full lg:w-1/2 p-3 lg:p-10'>
                     <img src="https://images.pexels.com/photos/5722158/pexels-photo-5722158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className='rounded-lg' alt="" />
                 </div>
-                <div className="container mx-auto max-w-4xl p-3 lg:p-10 ">
+                {formSubmitted ? (
+                    <motion.p
+                        className='text-white mt-4 text-center mx-auto'
+                        variants={fadeInUpVariants}
+                        initial='hidden'
+                        animate={inView ? 'visible' : 'hidden'}
+                        transition={{ type: 'spring', stiffness: 100, damping: 10, duration: 0.2, delay: 0.6 }}
+                    >
+                        Thank you! We will get to you.
+                    </motion.p>
+                ) : (<div className="container mx-auto w-full lg:w-1/2 p-3 lg:p-10 ">
                     <div className="mb-4">
                         {/* <label className="block text-sm font-semibold mb-1">Name:</label> */}
                         <input
@@ -137,8 +160,8 @@ const ConsultationForm = () => {
                     {showPreviousExperience && (
                         <div className="mb-4">
                             {/* <label className="block text-sm font-semibold mb-1 text-white">
-              :
-            </label> */}
+      :
+    </label> */}
                             <textarea
                                 className="w-full border-[#414141] border rounded-lg p-3 text-white text-sm bg-[#191919]"
                                 value={formData.previousExperience}
@@ -148,7 +171,8 @@ const ConsultationForm = () => {
                         </div>
                     )}
                     <Button onClick={handleSubmit} variant='navbar2'>Submit</Button>
-                </div>
+                </div>)}
+
             </div>
         </div>
     );
